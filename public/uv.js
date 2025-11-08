1c980c2383ed8da4df3b57913e27626c4a73f447
LvGames <166935104+Scripts3353@users.noreply.github.com>
2025-11-07 23:08:05 -0500

    Add initial configuration to uv.js

---
+++ public/uv.js
@@ -0,0 +1,12 @@
+{
+  "bare": "/bare/",
+  "sw": "/sw.js",
+  "config": {
+    "prefix": "/uv/",
+    "ws": false,
+    "assets": "/uv/assets/",
+    "theme": "default",
+    "manager": "default",
+    "audio": false
+  }
+}
